7ade1d1b8f04e8d7fda66af8367bbc8c7a3e84fe
Ting.ye <ting.ye@MacBook-Pro.local>
2020-07-08 17:27:31 +0800

    push

--- vue-sty/my-vue-router/src/main.js
+++ vue-sty/my-vue-router/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Model from './modelsUsed'
+import Model from 'vue-class-model'
 import Power from './models/power.js'
 
 Vue.config.productionTip = false
