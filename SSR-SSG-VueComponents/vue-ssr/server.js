85388379c61feac2fe039f7506c3de05f50367ae
Ting.ye <ting.ye@MacBook-Pro.local>
2020-08-20 15:05:12 +0800

    push

--- SSR-SSG-VueComponents/vue-ssr/server.js
+++ SSR-SSG-VueComponents/vue-ssr/server.js
@@ -1,18 +1,46 @@
 const Vue = require('vue')
-const renderer = require('vue-server-renderer').createRenderer()
+const fs = require('fs')
+const renderer = require('vue-server-renderer').createRenderer({
+    template: fs.readFileSync('./index.template.html','utf-8')
+})
+const express = require('express')
 
-const app = new Vue({
-    template: `
-        <div id="app">
-            <h1>{{ message }}</h1>
-        </div>
-    `,
-    data:{
-        message:'ytyt'
-    }
+const serve = express()
+
+serve.get('/',(req,res)=>{
+    const app = new Vue({
+        template: `
+            <div id="app">
+                <h1>{{ message }}</h1>
+            </div>
+        `,
+        data:{
+            message:'ytyt'
+        }
+    })
+     
+    renderer.renderToString(app, {
+        title: 'yqyq',
+        meta: `<meta name="description" content="yyj">`
+    },(err, html)=>{
+        if (err) {
+            return res.status(500).end('Internal Server Error.')
+        }
+        res.setHeader('Content-Type', 'text/html; charset=utf8')
+        // res.end(` 
+        // <!DOCTYPE html> 
+        // <html lang="en">
+        //     <head> 
+        //         <title>Hello</title> 
+        //         <meta charset="UTF-8">
+        //     </head>
+        //     <body>${html}</body>
+        // </html>
+        // `)
+        res.end(html)
+    })
 })
 
-renderer.renderToString(app, (err, html)=>{
-    if (err) throw err
-    console.log(html)
+serve.listen(3000,()=>{
+    console.log('server running at port 3000.')
 })
